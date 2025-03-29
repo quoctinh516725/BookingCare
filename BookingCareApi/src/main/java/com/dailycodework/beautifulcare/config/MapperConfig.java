b19c26776f61472d7ba31f2a01ab921e3ddd9d4c
VuNguyen76 <thanhvuttq@gmail.com>
2025-03-29 10:10:25 +0700

    Thêm cấu hình MapperConfig để quét và đăng ký các Mapper như Spring beans.

---
+++ BookingCareApi/src/main/java/com/dailycodework/beautifulcare/config/MapperConfig.java
@@ -0,0 +1,10 @@
+package com.dailycodework.beautifulcare.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.dailycodework.beautifulcare.mapper")
+public class MapperConfig {
+    // Cấu hình này đảm bảo tất cả các Mapper được quét và đăng ký như Spring beans
+} 
